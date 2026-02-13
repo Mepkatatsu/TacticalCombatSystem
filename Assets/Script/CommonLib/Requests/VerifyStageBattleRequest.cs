45f8002777601fb783e409d402d684c7e7a6d7f9
Mepkatatsu <mepkatatsu@gmail.com>
2026-02-13 16:37:26 +0900

    CommonLib: 전투 종료 후 검증 요청 Request, Response 추가
    AliveEntities: 임시 체크를 위해 살아남은 Entity의 Id와 체력을 가진 Tuple

---
+++ Assets/Script/CommonLib/Requests/VerifyStageBattleRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Script.CommonLib.Requests
+{
+    public sealed class VerifyStageBattleRequest
+    {
+        public ulong UserId { get; }
+        public string RequestId { get; }
+        public List<float> UpdateIntervals { get; }
+
+        public VerifyStageBattleRequest(ulong userId, string requestId, List<float> updateIntervals)
+        {
+            UserId = userId;
+            RequestId = requestId;
+            UpdateIntervals = updateIntervals;
+        }
+    }
+}
---
+++ Assets/Script/CommonLib/Responses/VerifyStageBattleResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Script.CommonLib.Responses
+{
+    public class VerifyStageBattleResponse
+    {
+        public TeamFlag Winner { get; set; }
+        public List<Tuple<uint, float>> AliveEntities { get; set; }
+            
+        // Deserialize용 생성자
+        public VerifyStageBattleResponse() { }
+    }
+}
